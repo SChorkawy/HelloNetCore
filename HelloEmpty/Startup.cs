5ae744dee5705d7812c5cfd6a9deb9ebe07d4587
clocadevstudent <schorkawy@cloca.com>
2017-05-03 11:54:10 -0400

    123

--- HelloEmpty/Startup.cs
+++ HelloEmpty/Startup.cs
@@ -16,15 +16,24 @@ namespace HelloEmpty
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
-            //Look for an index file inside of wwwroot.
-            app.UseDefaultFiles();
+            ////Look for an index file inside of wwwroot.
+            //app.UseDefaultFiles();
 
             app.UseStaticFiles();
+
+            app.UseMvc(config => {
+                config.MapRoute(
+                    name:"Default",
+                    template: "{controller}/{actions}/{id?}",
+                    defaults: new { controller = "App", actions = "Index" }
+                    );
+            });
         }
     }
 }
